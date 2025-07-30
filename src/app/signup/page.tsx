51b0dc1dc786d50062d85157dbc9436d1ffbaf88
M. Sageer Hamisu <msabdhallah@gmail.com>
2025-07-30 10:46:43 +0000

    the background image in public folder is bg.jpg

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,8 +5,7 @@ export default function SignupPage() {
     <main className="relative flex min-h-screen flex-col items-center justify-center p-4">
        <div 
         className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: "url('https://placehold.co/1920x1080.png')", filter: 'blur(2px) brightness(0.4)' }}
-        data-ai-hint="leafy greens"
+        style={{ backgroundImage: "url('/bg.jpg')", filter: 'blur(2px) brightness(0.4)' }}
       ></div>
       <SignupForm />
     </main>
